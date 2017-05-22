45a55149e033542a66e426db7701c8fa49d392a4
XuZhang <241864330@qq.com>
2017-05-22 20:51:28 +0800

    string is palindrome,by recursion

---
+++ MIT_600/stringPalRe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+####################################################################
+#
+#  test palindrome string by recursion
+#
+####################################################################
+
+var = input("enter string: ")
+
+def testPal(var):
+    def toChar(string):
+        ans = ''
+        for c in string:
+            if c in "abcdefghijklmnopqrstuvwxyz0123456789":
+                ans += c
+        return ans
+    def isPal(string):
+        if len(string) <= 1:
+            return True
+        else:
+            return string[0] == string[-1] and isPal(string[1:-1])
+    return isPal(toChar(var))
+print(testPal(var))
